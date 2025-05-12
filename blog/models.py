bfd04ebf614d7db9d52beb5e042597e6ed9f8bfb
Amro Mentash <mentash@gmail.com>
2025-05-12 06:53:22 +0300

    Add `Post` model and initial migration for blog app
    Introduce a `Post` model with fields for title, slug, author, body, publish date, and status, along with database indexing and ordering. Register the blog app in the settings and generate the initial migration to set up the required database schema.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
+from django.db.models.functions import Now
 
-# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey( # many-to-one
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts', # author.blog_posts
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    # Use database-computed default values using database function
+    # publish = models.DateTimeField(db_default=Now())
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices, # [('DF', 'Draft'), ('PB', 'Published')]
+        default=Status.DRAFT,
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+
+    def __str__(self):
+        return self.title
+
